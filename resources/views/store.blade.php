d97c752390255ab3b7cb99674e2d5a568b009f7f
Arade Anthony <anthonyarade@gmail.com>
2025-12-01 15:43:38 -0400

    fix #3

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Boutique')
 
 @section('hero-section')
-
+<h1 class="text-4xl font-bold heading-font text-third text-center mb-4">Our Book Collection</h1>
+<p class="text-xl text-third text-center max-w-3xl mx-auto">Discover thousands of books across all genres. Find your next great read in our carefully curated collection.</p>
+@endsection
 
 @section('content')
     <!-- Breadcrumb -->
